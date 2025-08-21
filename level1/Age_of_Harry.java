3f0a92b2ea904c06df6dc6f521c4274b4e050d82
gavicka1 <nikhil.sharma9149311757@gmail.com>
2025-08-21 20:26:06 +0530

    level 1 first question

---
+++ level1/Age_of_Harry.java
@@ -0,0 +1,8 @@
+public class Age_of_Harry{
+    public static void main(String[] args) {
+        int birth = 2000;
+        int current = 2024;
+        int age = current - birth;
+        System.err.println(" Harry's age in 2024 is " + age );
+    }
+}
