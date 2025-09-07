a8d892f621e301cf5922e4826fe2abae466c1703
gavicka1 <nikhil.sharma9149311757@gmail.com>
2025-09-07 22:34:51 +0530

    com

---
+++ day1programmingelements/level1/Age_of_Harry.java
@@ -0,0 +1,12 @@
+public class Age_of_Harry{
+    public static void main(String[] args) {
+        int birth = 2000;
+        int current = 2024;
+        int age = current - birth;
+        System.err.println(" Harry's age in 2024 is " + age );
+    }
+}
+
+//git add .
+// git commit -m "sdkhfskjd"
+// git puch origin core-java
---
+++ day1programmingelements/level1/ques10.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ques10 {
+    public static void main(String[] args) {
+        double heightCm;
+        Scanner input = new Scanner(System.in);
+        heightCm = input.nextDouble();
+        double totalInches = heightCm / 2.54;
+        int feet = (int) (totalInches / 12);
+        double inches = totalInches % 12;
+        System.out.println("Your Height in cm is " + heightCm + " while in feet is " + feet + " and inches is " + inches);
+    }
+}
